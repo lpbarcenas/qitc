a6d2d83d5c31043892f4a7d40cfb1bd27daf9012
insignia92 <isukamona@ymail.com>
2012-12-09 22:10:03 -0800

    New Template structure

--- application/views/includes/template.php
+++ application/views/includes/template.php
@@ -1,17 +1,25 @@
 <?php
-
-if(empty($main_content)){
-	$this->load->view('includes/header');
-	$this->load->view('public/home');
-	$this->load->view('includes/footer');
-}else if($main_content == 'admin/adminHome' || $main_content == 'admin/announcements' || $main_content == 'admin/contentManager' ){
-	$this->load->view('includes/adminHeader');
-	$this->load->view('admin/navigation');
-	$this->load->view($main_content);
-	$this->load->view('includes/footer');
-}else if($main_content == 'admin/login_form'){
-	$this->load->view('includes/loginHeader');
-	$this->load->view($main_content);
-	$this->load->view('includes/footer');
+switch($main_content){
+	case 'admin/contentManager':
+	case 'admin/announcements':
+	case 'admin/adminHome':
+			$this->load->view('includes/adminHeader');
+			$this->load->view('admin/navigation');
+			$this->load->view($main_content);
+			$this->load->view('includes/footer');
+	break;
+	case 'admin/login_form':{
+		
+		$this->load->view('includes/loginHeader');
+		$this->load->view($main_content);
+		$this->load->view('includes/footer');
+		
+	}break;
+	case '':
+	case 'public/home':
+		$this->load->view('includes/header');
+		$this->load->view('public/home');
+		$this->load->view('includes/footer');
+	break;
 }
 
