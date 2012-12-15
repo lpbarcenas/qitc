78dc3405eed32bfffe87361060102158abca8367
Joshua Paylaga <joshuapaylaga@gmail.com>
2012-12-15 18:42:48 +0800

    Initial commit on the registration controller

---
+++ application/controllers/public/registration.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Registration extends CI_controller {
+	
+	public function index(){
+		
+		// CONTENT HERE
+	}
+}
